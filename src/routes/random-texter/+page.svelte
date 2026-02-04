b5a0289856d30e8ef5695e7e47ea8b853cff71f2
Joern Bernhardt <joern.bernhardt@googlemail.com>
2026-02-04 02:10:00 +0100

    feat: Add an example generator
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/random-texter/+page.svelte
+++ src/routes/random-texter/+page.svelte
@@ -3,7 +3,21 @@
 	import FullWidthSection from '$lib/common/FullWidthSection.svelte';
 	import PageLayout from '$lib/common/PageLayout.svelte';
 	import { m } from '$lib/paraglide/messages';
-	import { getLocale } from '$lib/paraglide/runtime';
+
+	let sentenceParts = $state([
+		['Created', 'Made'],
+		['with love', 'under pressure'],
+		['for the best audience', 'for humanity', 'without hesitation', ''],
+		['by a horde of apes', 'by someone who cares']
+	]);
+
+	let randomSentence = $derived(
+		sentenceParts.reduce((result, part) => {
+			const next = part[Math.floor(Math.random() * part.length)];
+			if (!next) return result;
+			return `${result} ${next}`;
+		}, '')
+	);
 </script>
 
 <PageLayout
@@ -12,7 +26,19 @@
 	title={m['tools.randomTexter.title']()}
 	description={m['tools.randomTexter.seoDescription']()}
 >
-	<FullWidthSection class="grid-rows-[max-content_1fr]">
+	<FullWidthSection class="grid-rows-[max-content_1fr] gap-4">
 		<p>Trying to recreate a simple random text generator</p>
+		<ol class="flex flex-row gap-2">
+			{#each sentenceParts as sentencePartTexts, columnIndex}
+				<li>
+					<ul class="grid">
+						{#each sentencePartTexts as _part, cellIndex}
+							<li><input bind:value={sentenceParts[columnIndex][cellIndex]} /></li>
+						{/each}
+					</ul>
+				</li>
+			{/each}
+		</ol>
+		<p>{randomSentence}</p>
 	</FullWidthSection>
 </PageLayout>
