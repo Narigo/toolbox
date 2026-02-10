7107eedf8112dd1cc832be793d2d713e908536cd
Joern Bernhardt <joern.bernhardt@googlemail.com>
2026-02-11 00:05:26 +0100

    feat: Add German text example
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/random-texter/+page.svelte
+++ src/routes/random-texter/+page.svelte
@@ -3,13 +3,24 @@
 	import FullWidthSection from '$lib/common/FullWidthSection.svelte';
 	import PageLayout from '$lib/common/PageLayout.svelte';
 	import { m } from '$lib/paraglide/messages';
+	import { getLocale } from '$lib/paraglide/runtime';
 
-	let sentenceParts = $state([
+	const initialDe = [
+		['Mit Liebe', 'Unter hohem Druck'],
+		['für das beste Publikum', 'für die Menschheit', 'ohne zu zweifeln', ''],
+		['von einer Horde Affen', 'von jemandem, dem es wichtig ist,'],
+		['erschaffen', 'gebaut']
+	];
+	const initialEn = [
 		['Created', 'Made'],
 		['with love', 'under pressure'],
 		['for the best audience', 'for humanity', 'without hesitation', ''],
 		['by a horde of apes', 'by someone who cares']
-	]);
+	];
+
+	const initial = getLocale() === 'de' ? initialDe : initialEn;
+
+	let sentenceParts = $state(initial);
 
 	let randomSentence = $derived(
 		sentenceParts.reduce((result, part) => {
